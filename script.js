8462333997d0c0dbbebe02b96991f92b8da50fee
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-09-11 12:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+let arr = [];
+let regex = /\bthe \b|\ba \b|\ban \b/gi;
+for(let index of touristSpots) {
+	arr[index] = touristSpots[index].replace(regex, "");
+}
+touristSpots.sort();
+let map = {
+	'Virupaksha Temple' : 'The Virupaksha Temple',
+	'Victoria Memorial' : 'Victoria Memorial',
+	'Tajmahal' : 'Tajmahal',
+}
+for (let index of arr) {
+arr[index] = map[index];	
+}
+console.log(arr)
