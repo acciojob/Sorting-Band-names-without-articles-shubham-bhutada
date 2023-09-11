11d1e6d9b11f744e1f013c17c6aa63b0791a700a
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-09-11 14:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,27 +2,23 @@
 
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-let arr = [];
-// let regex = /\bthe \b|\ba \b|\ban \b/gi;
-// for(let index of touristSpots) {
-// 	arr[index] = touristSpots[index].replace(regex, "");
-// }
-let regex = /\bthe \b|\ba \b|\nan \b/gi;
+let regex = /\bthe \b|\ba \b|\ban \b/gi;
 for (let i = 0; i < touristSpots.length; i++) {
-  arr[i] = touristSpots[i].replace(regex, "");
+  touristSpots[i] = touristSpots[i].replace(regex, "").trim();
 }
+
 touristSpots.sort();
+
 let map = {
-	'Virupaksha Temple' : 'The Virupaksha Temple',
-	'Victoria Memorial' : 'Victoria Memorial',
-	'Tajmahal' : 'Tajmahal',
-}
-// for (let index of arr) {
-// arr[index] = map[index];	
-// }
-for (let i = 0; i < arr.length; i++) {
-  if (map[arr[i]]) {
-    arr[i] = map[arr[i]];
+  'The Virupaksha Temple': 'Virupaksha Temple',
+  'Victoria Memorial': 'Victoria Memorial',
+  'Tajmahal': 'Tajmahal'
+};
+
+for (let i = 0; i < touristSpots.length; i++) {
+  if (map[touristSpots[i]]) {
+    touristSpots[i] = map[touristSpots[i]];
   }
 }
-console.log(arr)
+
+console.log(touristSpots);
